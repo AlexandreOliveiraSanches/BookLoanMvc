29bb86bd2a876cbc16e5884b1fb64a6b2936658b
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-10-22 10:44:10 -0300

    Criação do projeto de Empréstimos de Livros com Asp.Net Core Mvc

---
+++ BookLoanMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookLoanMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
