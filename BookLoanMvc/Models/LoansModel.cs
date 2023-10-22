a462efd8a647c41474e05492285097f593e6507f
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-10-22 11:21:08 -0300

    Criação do 'LoansModel' e instalação de pacotes

---
+++ BookLoanMvc/Models/LoansModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookLoanMvc.Models
+{
+    public class LoansModel
+    {
+        public int Id { get; set; }
+        public string Recipient { get; set; }
+        public string Lender { get; set; }
+        public string Book { get; set; }
+        public DateTime LastUpdate { get; set; } = DateTime.Now;
+    }
+}
