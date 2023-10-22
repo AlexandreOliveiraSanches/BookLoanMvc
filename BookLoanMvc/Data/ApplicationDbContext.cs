e2355074cd82b0f900de80d7a7e730ea1cd4896b
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-10-22 12:53:55 -0300

    Ligação com banco de dados SQL Server

---
+++ BookLoanMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookLoanMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLoanMvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<LoansModel> Loans { get; set; }
+
+    }
+}
